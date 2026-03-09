c293f2c5e927022a01b5d139e3b57243f8c59d6b
Lucas-Fabri <dev.cunhaww@gmail.com>
2026-03-09 14:50:33 -0300

    feat: add knexfile configuration

---
+++ knexfile.ts
@@ -0,0 +1,11 @@
+export default {
+  client: 'sqlite3',
+  connection: {
+    filename: './src/database/db.sqlite',
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extensions: 'ts',
+    directory: './src/database/migrations',
+  },
+};
