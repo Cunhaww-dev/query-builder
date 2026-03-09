15c1a3e66d15b6b50f8498cd4008d15c0ec0b3a4
Lucas-Fabri <dev.cunhaww@gmail.com>
2026-03-09 17:39:52 -0300

    feat: create POST endpoint for insert courses and import knex database connection file

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import express from 'express';
 import type { Request, Response } from 'express';
+import { knex } from './database/knex'; // importação do knex para usar a conexão com o banco de dados
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
 app.get('/', async (req: Request, res: Response) => {
-   res.json({ message: "Hello World :)"})
-})
+  res.json({ message: 'Hello World :)' });
+});
 
-app.listen(3333, () => console.log('Server is running on port 3333'))
+app.post('/courses', async (req: Request, res: Response) => {
+  const { name } = req.body;
+
+  await knex('courses').insert({ name }); // inserindo um novo curso na tabela "courses" do banco de dados
+  res.status(210).json({ name });
+});
+
+app.listen(3333, () => console.log('Server is running on port 3333'));
