e5cba8dc7924ebb62ede1148fa073117121475d7
Lucas-Fabri <dev.cunhaww@gmail.com>
2026-03-09 15:38:33 -0300

    fix: update database filename path for WSL compatibility

--- knexfile.ts
+++ knexfile.ts
@@ -1,7 +1,8 @@
 export default {
   client: 'sqlite3',
   connection: {
-    filename: './src/database/db.sqlite',
+    // filename: './src/database/db.sqlite',
+    filename: '/mnt/c/dev/db/db.sqlite', // caminho absoluto para o banco de dados, necessário para o WSL acessar o arquivo do Windows
   },
   useNullAsDefault: true,
   migrations: {
