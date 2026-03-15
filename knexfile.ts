8d56582ab7feca769156194537552a53eeb15e56
Lucas-Fabri <dev.cunhaww@gmail.com>
2026-03-15 19:06:58 -0300

    feat: enable foreign key support in SQLite configuration

--- knexfile.ts
+++ knexfile.ts
@@ -4,6 +4,12 @@ const config = {
     // filename: './src/database/sqlite.db',
     filename: '/mnt/c/dev/db/database.db', // caminho absoluto para o banco de dados, necessário para o WSL acessar o arquivo do Windows
   },
+  pool: {
+    afterCreate: (connection: any, done: any) => {
+      connection.run('PRAGMA foreign_keys = ON'); // Habilitando o suporte a chaves estrangeiras no SQLite, garantindo que as relações entre as tabelas sejam respeitadas.
+      done();
+    },
+  },
   useNullAsDefault: true,
   migrations: {
     extensions: 'ts',
